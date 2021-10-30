8d4dd2edba2aeafbd4125d6b5a4c762dc6d31e65
clemens2206 <clekerber@tsn.at>
2021-10-30 15:54:12 +0200

    Klasse Employee

---
+++ Spring-boot-tutorial-master/src/main/java/com/dailycodebuffer/Springboot/tutorial/entity/Employee.java
@@ -0,0 +1,26 @@
+package com.dailycodebuffer.Springboot.tutorial.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long employeeId;
+
+    private String employeeFirstName;
+    private String employeeLastName;
+}
